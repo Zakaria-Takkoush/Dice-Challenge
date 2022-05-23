02ed2efdea287085c32349e2d6775039e7318919
Zakaria <zaktakk96@gmail.com>
2022-05-23 10:54:20 +0300

    V5 - JS - Grabbed header element and created comparision function

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.onload = function () {
 
 // create 2 random variables between 1 and 6    
-    var rand1 = Math.floor(Math.random() * 6) + 1;      // added 1 to start from 1 and neglect 0
-    var rand2 = Math.floor(Math.random() * 6) + 1;
+    var rand1 = Math.floor(Math.random() * 6) + 1      // added 1 to start from 1 to 6 and neglect 0
+    var rand2 = Math.floor(Math.random() * 6) + 1
 
 // Get image objects
     var dice1 = document.getElementsByClassName("img1")[0]
@@ -12,5 +12,20 @@ window.onload = function () {
     dice1.setAttribute("src", "images/dice" + rand1 + ".png")
     dice2.setAttribute("src", "images/dice" + rand2 + ".png")
 
+// Get Header element:
+    var result = document.getElementsByTagName("h1")[0]
+
+// Comparision function
+    if (rand1 > rand2 ){        // Player 1 gets a hugher die roll
+        result.innerText = "🚩 Player 1 Wins!"
+    }
+
+    else if (rand1 < rand2 ){   // Player 2 gets a hugher die roll
+        result.innerText = "🚩 Player 2 Wins!"
+    }
+
+    else {                      // Both players get an equal die roll
+        result.innerText = "Draw"
+    }
 }
 
