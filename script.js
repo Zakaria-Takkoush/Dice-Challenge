cef67d21066735e3ce9b44be3549e713955f7b98
Zakaria <zaktakk96@gmail.com>
2022-05-23 10:24:30 +0300

    V2 - Created 2 random variables for the dice

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+
+// create 2 random variables between 1 and 6    
+    var rand1 = Math.floor(Math.random() * 6) + 1;      // added 1 to start from 1 and neglect 0
+    var rand2 = Math.floor(Math.random() * 6) + 1;
+
+
+}
+
